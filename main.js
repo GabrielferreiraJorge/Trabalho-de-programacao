fe912a8bda4adb2afc888a66ff89038355dd1cee
GabrielferreiraJorge <130074604+GabrielferreiraJorge@users.noreply.github.com>
2023-10-23 08:37:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function tocaSomPom (id Elemento Audio) {
 
 const listadeTeclas = document.querySelectorAll('.tecla_pom');
 let contador = 0,
-while(contador<9){
+while(contador<lista de teclas.lenght){
     listadeTeclas[contador].onclick=tocaSomPom;
     contador = contador + 1;
 }
